730e02cc443c93011869e0364c4190d4481d16de
Nan Zheng <nanzhen@amazon.com>
2019-10-17 11:44:21 +0800

    Add new reference

---
+++ static/BRDDetails/support/test_data.js
@@ -0,0 +1,71 @@
+var nothing = [
+  {
+    "sectionId": "1",
+    "comments": [
+      {
+        "id": 88,
+        "authorAvatarUrl": "support/images/jon_snow.png",
+        "authorName": "Jon Sno",
+        "authorId": 1,
+        "authorUrl": "http://en.wikipedia.org/wiki/Kit_Harington",
+        "comment": "I'm Ned Stark's bastard",
+        "replies": [
+          {
+            "id" : 100,
+            "authorAvatarUrl": "support/images/jon_snow.png",
+            "authorName": "Jon Sno",
+            "authorId": 1,
+            "authorUrl": "http://en.wikipedia.org/wiki/Kit_Harington",
+            "comment": "P.S.: I know nothing.",
+            "parentId": 88
+          }
+        ]
+      }
+    ]
+  }
+];
+
+var oneComment = [
+  {
+    "sectionId": "003",
+    "comments": [
+      {
+        "id": 88,
+        "authorAvatarUrl": "static/img/user.png",
+        "authorName": "nanzhen",
+        "authorId": 1,
+        "authorUrl": "http://en.wikipedia.org/wiki/Kit_Harington",
+        "comment": "Some issues.",
+        "replies": [
+        ]
+      }
+    ]
+  }
+];
+
+var twoComments = [
+  {
+    "sectionId": "003",
+    "comments": [
+      {
+        "id": 88,
+        "authorAvatarUrl": "static/img/user.png",
+        "authorName": "nanzhen",
+        "authorId": 1,
+        "authorUrl": "http://en.wikipedia.org/wiki/Kit_Harington",
+        "comment": "Some issues.",
+        "replies": [
+          {
+            "id" : 100,
+            "authorAvatarUrl": "static/img/user.png",
+            "authorName": "PM",
+            "authorId": 1,
+            "authorUrl": "http://en.wikipedia.org/wiki/Kit_Harington",
+            "comment": "Resolved.",
+            "parentId": 88
+          }
+        ]
+      }
+    ]
+  }
+];
