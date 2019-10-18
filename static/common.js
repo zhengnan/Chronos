1e5c150aae43ebc8fa22a3c7339c2267ccbe6c89
Wenfeng Zhao <wenfenz@amazon.com>
2019-10-18 15:20:49 +0800

    send notification mail to dev

--- static/common.js
+++ static/common.js
@@ -16,7 +16,28 @@ function onBRDPageApproveButtonClicked() {
     window.location.href = 'brddetailn.html';
 }
 
+function sendMail(subject, message){
+    var data_js = {};
+    data_js['from'] = 'wenfenz@amazon.com';
+    data_js['to'] = 'nanzhen@amazon.com';
+    data_js['subject'] = subject;
+    data_js['text'] = message;
+
+    var params = toParams(data_js);
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "sendmail.html?" + params, true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.send(params);
+}
+
+
 function onBRDPageNextButtonClicked() {
+	var subject = '[chronos] NOTICE - Dev task starts';
+	var content = 'See details at http://dev-dsk-nanzhen-2b-443e6702.us-west-2.amazon.com:5000/devtimeline.html';
+	sendMail(subject, content);
+
 	window.location.href = 'timeline.html';
 }
 
