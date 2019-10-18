c751c78a14db730ee463c5332ff93f457a094275
Nan Zheng <nanzhen@amazon.com>
2019-10-18 06:58:28 +0000

    fix bugs

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def get_parsed_approver_items():
 if __name__ == '__main__':
     # app.run(host, port, debug, options)
     # 默认值：host=0.0.0.0, port=5000, debug=false
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
